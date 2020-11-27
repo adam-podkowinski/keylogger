2b78e90fce39d9300804bbbac17bf528369b404e
adampodkowinski <adampodkdev@gmail.com>
2020-11-27 23:01:47 +0100

    Configure cmake

---
+++ build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/WindowsProfiler.cpp.obj"
+  "Tutorial.exe"
+  "Tutorial.exe.manifest"
+  "Tutorial.pdb"
+  "libTutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/WindowsProfiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WindowsProfiler.dir/WindowsProfiler.cpp.obj"
+  "WindowsProfiler.exe"
+  "WindowsProfiler.exe.manifest"
+  "WindowsProfiler.pdb"
+  "libWindowsProfiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WindowsProfiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
